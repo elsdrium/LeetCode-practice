6b2114223cc75d5de6e81c2398298bba96971adf
elsdrium <elsdrium@gmail.com>
2017-05-09 21:13:07 +0800

    Solved #554

---
+++ 554-Brick_Wall.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int leastBricks(vector<vector<int>>& wall) {
+        int sum;
+        unordered_map<int, int> umap;
+        for ( auto& row : wall )
+            for ( int i=0, sum=0; i!=row.size()-1; ++i ) {
+                sum += row[i];
+                umap[sum] = umap.find(sum) == umap.end() ? 1 : umap[sum]+1;
+            }
+        return wall.size() - (umap.empty() ? 0 : max_element(umap.begin(), umap.end(),
+                [&](const pair<int, int>& x, pair<const int, int>& y){return x.second < y.second;}
+                )->second);
+    }
+};
