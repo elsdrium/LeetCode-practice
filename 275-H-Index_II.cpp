05c1449858e2949bafb01848bf33a150290f41c2
elsdrium <elsdrium@gmail.com>
2017-05-20 20:20:58 +0800

    Solved #275

---
+++ 275-H-Index_II.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int hIndex(vector<int>& citations) {
+        if ( citations.empty() ) return 0;
+        long long s = 0, t = citations.size()-1, m;
+        while ( t - s > 1 ) {
+            m = ( s + t ) / 2;
+            if ( citations[m] >= citations.size() - m ) {
+                if ( citations[m-1] <= citations.size() - m ) s = m;
+                else t = m;
+            }
+            else s = m;
+        }
+        if ( citations[t] >= citations.size()-t && citations[t-1] <= citations.size()-t )
+            return citations.size() - t;
+        else if ( citations[s] >= citations.size()-s && citations[s-1] <= citations.size()-s )
+            return citations.size() - s;
+        return 0;
+    }
+};
