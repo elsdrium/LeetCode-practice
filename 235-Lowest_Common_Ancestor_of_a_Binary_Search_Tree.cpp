9a8078bb2f0dc23e8842a663688ed5a3e0fb375e
elsdrium <elsdrium@gmail.com>
2017-05-21 17:26:17 +0800

    Solved #235

---
+++ 235-Lowest_Common_Ancestor_of_a_Binary_Search_Tree.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        if ( p == q ) return p;
+        while ( (root->val - p->val)*(root->val - q->val) > 0 ) {
+            root = root->val - p->val > 0 ? root->left : root->right;
+        }
+        return root;
+    }
+};
