a045b225b3227ba5f4394c24fb2e21508ec6b05e
elsdrium <elsdrium@gmail.com>
2017-06-03 01:23:50 +0800

    Revised #263

--- 263-Ugly_Number.cpp
+++ 263-Ugly_Number.cpp
@@ -2,11 +2,9 @@ class Solution {
 public:
     bool isUgly(int num) {
         if ( !num ) return false;
-        while ( !(num % 2) || !(num % 3) || !(num % 5) ) {
-            if ( !(num % 2) ) num /= 2;
-            if ( !(num % 3) ) num /= 3;
-            if ( !(num % 5) ) num /= 5;
-        }
+        while ( !(num % 2) ) num /= 2;
+        while ( !(num % 3) ) num /= 3;
+        while ( !(num % 5) ) num /= 5;
         return num == 1;
     }
 };
