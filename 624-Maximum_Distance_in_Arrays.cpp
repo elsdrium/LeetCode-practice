6a98f28a3a2cba76a2f082065de26767ae46ba05
elsdrium <elsdrium@gmail.com>
2017-06-19 04:13:03 +0800

    Revised #624 to O(n) time complexity

--- 624-Maximum_Distance_in_Arrays.cpp
+++ 624-Maximum_Distance_in_Arrays.cpp
@@ -1,14 +1,12 @@
 class Solution {
 public:
     int maxDistance(vector<vector<int>>& arrays) {
-        int result = 0;
-        for ( int i=0; i!=arrays.size(); ++i ) {
-            for ( int j=i+1; j!=arrays.size(); ++j ) {
-                if ( abs(arrays[i].front()-arrays[j].back()) > result )
-                    result = abs(arrays[i].front()-arrays[j].back());
-                if ( abs(arrays[j].front()-arrays[i].back()) > result )
-                    result = abs(arrays[j].front()-arrays[i].back());
-            }
+        int low = arrays[0].front(), high = arrays[0].back(), result = 0;
+        for ( int i=1; i!=arrays.size(); ++i ) {
+            result = max(high - arrays[i].front(), result);
+            result = max(arrays[i].back() - low, result);
+            low = min(arrays[i].front(), low);
+            high = max(arrays[i].back(), high);
         }
         return result;
     }
