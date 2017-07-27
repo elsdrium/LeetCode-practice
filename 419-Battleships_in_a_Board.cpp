e87282b32fe675f16754139191572ac15987235d
elsdrium <elsdrium@gmail.com>
2017-07-27 17:47:13 +0800

    Solved #419 in one-pass, using only O(1) space

---
+++ 419-Battleships_in_a_Board.cpp
@@ -0,0 +1,14 @@
+ass Solution {
+public:
+    int countBattleships(vector<vector<char>>& board) {
+        int result = 0;
+        for ( int i = 0; i < board.size(); ++i )
+            for ( int j = 0 ; j < board[0].size(); ++j ) {
+                if ( board[i][j] != 'X' ) continue;
+                if ( i && board[i-1][j] == 'X' ) continue;
+                if ( j && board[i][j-1] == 'X' ) continue;
+                ++result;
+            }
+        return result;
+    }
+};
